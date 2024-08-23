082b7a6e551cff8ae96afda86cb8e56d103001de
EvGeN180 <malyna.evgen2010@gmail.com>
2024-08-23 10:32:32 +0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,33 @@
+#task1
+# students = [
+#     ('Alice',87)
+#     ('Bob',87)
+#     ('Mark',65)
+#     ('Evgen',94)
+# ]
+
+# result = {}
+
+# for student,grade in students:
+#     if grade in result:
+#         result[grade] = [student]
+#     else:
+#         result[grade] = [student]
+# print(result)
+
+
+# task2
+
+# students = {'Alice':87,'Bob':87,'Mark':65,'Evgen':94}
+# student1 = students['Alice']
+# student2 = students['Bob']
+# student3 = students['Evgen']
+# student4= students['Mark']
+# result = student1+student2+student3+student4/4
+# print(result)
+
+
+# task3
+
+# tuple1 = (57,46,17.199,230,47)
+# print(sum(tuple1)/len(tuple1))
