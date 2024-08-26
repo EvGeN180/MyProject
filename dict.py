95740f17c4d86bb3f0d7bf673b93559ebfae606b
EvGeN180 <malyna.evgen2010@gmail.com>
2024-08-26 12:25:00 +0300

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,39 @@
+# def f(dictionary, key):
+#     if key in dictionary:
+#         print("True")
+#     else:
+#         print("False")
+# my_dict = {'a': 1, 'b': 2, 'c': 3}
+# f(my_dict, 'a') 
+
+
+
+
+# def func(dictionary, value):
+#     keyss = []
+#     for key, val in dictionary.items():
+#         if val == value:
+#             keyss.append(key)
+#     return keyss
+# my_dict = {'a': 1, 'b': 2, 'c': 2}
+# print(func(my_dict, 2)) 
+
+
+
+
+# student = {
+#     'name': 'Mark',
+#     'surname': 'Markiv',
+#     'age': 21,
+#     'faculty': 'science'
+# }
+
+# name = student['name']
+# faculty = student['faculty']
+# print(f"name: {name}")
+# print(f"faculty: {faculty}")
+
+# student.update({'average_mark' : 11})
+
+# print("new dictionary:")
+# print(student)
