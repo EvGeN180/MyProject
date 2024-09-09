60d235e2dd42fff688f23edc7a9d0ea4d586a3e5
EvGeN180 <malyna.evgen2010@gmail.com>
2024-09-09 11:14:17 +0300

    Add files via upload

---
+++ dzclass.py
@@ -0,0 +1,37 @@
+# class Person:
+#     def __init__(self, name, surname,age):
+#         self.name = name
+#         self.surname = surname
+#         self.age = age
+
+#     def showInfo(self):
+#         print(f"name: {self.name}, surname: {self.surname}, age {self.age}")
+
+
+# name = input("Enter your name ")
+# surname = input("Enter your surname ")
+# age = input("Enter your age ")
+# obj = Person(name,surname,age)
+# obj.showInfo()
+
+
+
+
+# class Rectangle:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+
+#     def s_rectangle (self):
+#         return self.width * self.height
+
+#     def p_rectangle (self):
+#         return 2 * (self.width + self.height)
+
+#     def showInfo(self):
+#         print(f"width: {self.width}, height: {self.height}")
+
+# obj = Rectangle(30, 50)
+# obj.showInfo()
+# print(f"S: {obj.s_rectangle()}")
+# print(f"P: {obj.p_rectangle()}")
